b42d1e40eee8c1fb5e5016bf88dd5c7d355d0786
ananikic <you@example.com>
2020-04-10 17:02:32 +0300

    Fixed page component test

--- src/app/kanban/page/page.component.spec.ts
+++ src/app/kanban/page/page.component.spec.ts
@@ -6,23 +6,27 @@ import { PageService } from '../kanban-services/page.service';
 import { BoardService } from '../kanban-services/board.service';
 import { ActivatedRoute } from '@angular/router';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { By } from '@angular/platform-browser';
 
 describe('Component: PageComponent', () => {
   let pageServiceSpy = jasmine.createSpyObj('pageService', ['sortBoards', 'findUserPageById']);
   let boardServiceSpy = jasmine.createSpyObj('boardService', ['findBoards']);
-  let routeSpy = {paramMap: jasmine.createSpyObj('paramMap', {'get': of({id: '123'}), 'subscribe': null })};
+  let routeSpy = { paramMap: jasmine.createSpyObj('paramMap', { 'get': of({ id: '123' }), 'subscribe': null }) };
+  const matDialogSpy = jasmine.createSpyObj('dialog', ['open']);
   let component: PageComponent;
   let fixture: ComponentFixture<PageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PageComponent ],
-      providers: [{provide: PageService, useValue: pageServiceSpy}, 
-        {provide: BoardService, useValue: boardServiceSpy},
-        {provide: ActivatedRoute, useValue: routeSpy}],
+      declarations: [PageComponent],
+      providers: [{ provide: PageService, useValue: pageServiceSpy },
+      { provide: BoardService, useValue: boardServiceSpy },
+      { provide: ActivatedRoute, useValue: routeSpy },
+      { provide: MatDialog, useValue: matDialogSpy }],
       schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -34,4 +38,13 @@ describe('Component: PageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should open dialog on button click', () => {
+    let buttonDe  = fixture.debugElement.query(By.css('.board-btn'));
+    let buttonEl = buttonDe.nativeElement;
+    buttonEl.click();
+    expect(matDialogSpy.open.calls.count()).toBe(1, 'spy method called once');
+  });
+
+  
 });
