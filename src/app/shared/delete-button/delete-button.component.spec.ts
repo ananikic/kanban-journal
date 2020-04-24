26e786e8b2424229720c73607e550a44d30c6c28
ananikic <you@example.com>
2020-04-24 08:05:24 +0300

    Fixed Delete button component test

--- src/app/shared/delete-button/delete-button.component.spec.ts
+++ src/app/shared/delete-button/delete-button.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteButtonComponent } from './delete-button.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('DeleteButtonComponent', () => {
+describe('Component: DeleteButtonComponent', () => {
   let component: DeleteButtonComponent;
   let fixture: ComponentFixture<DeleteButtonComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DeleteButtonComponent ]
+      declarations: [ DeleteButtonComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
